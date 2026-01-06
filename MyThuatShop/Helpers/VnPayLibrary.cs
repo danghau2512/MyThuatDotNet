4b663afc9e88465e80ea9d96a50cd0af348ba107
danghau2512 <23130095@st.hcmuaf.edu.vn>
2026-01-06 10:08:51 +0700

    cap nhat vnpaylibrary

--- MyThuatShop/Helpers/VnPayLibrary.cs
+++ MyThuatShop/Helpers/VnPayLibrary.cs
@@ -1,14 +1,16 @@
 ﻿using System.Globalization;
+using System.Net.Sockets;
 using System.Net;
 using System.Security.Cryptography;
 using System.Text;
 
-namespace MyThuatShop.Helpers 
+namespace MyThuatShop.Models
 {
+
     public class VnPayLibrary
     {
-        private SortedList<string, string> _requestData = new SortedList<string, string>(new VnPayCompare());
-        private SortedList<string, string> _responseData = new SortedList<string, string>(new VnPayCompare());
+        private readonly SortedList<string, string> _requestData = new SortedList<string, string>(new VnPayCompare());
+        private readonly SortedList<string, string> _responseData = new SortedList<string, string>(new VnPayCompare());
 
         public void AddRequestData(string key, string value)
         {
@@ -31,52 +33,117 @@ namespace MyThuatShop.Helpers
             return _responseData.TryGetValue(key, out var retValue) ? retValue : string.Empty;
         }
 
-        public string CreateRequestUrl(string baseUrl, string vnp_HashSecret)
+        #region Request
+        public string CreateRequestUrl(string baseUrl, string vnpHashSecret)
         {
-            StringBuilder data = new StringBuilder();
-            foreach (KeyValuePair<string, string> kv in _requestData)
+            var data = new StringBuilder();
+
+            foreach (var (key, value) in _requestData.Where(kv => !string.IsNullOrEmpty(kv.Value)))
             {
-                if (data.Length > 0)
-                {
-                    data.Append("&");
-                }
-                data.Append(kv.Key + "=" + WebUtility.UrlEncode(kv.Value));
+                data.Append(WebUtility.UrlEncode(key) + "=" + WebUtility.UrlEncode(value) + "&");
             }
-            string queryString = data.ToString();
 
-            string vnp_SecureHash = Utils.HmacSHA512(vnp_HashSecret, queryString);
-            string paymentUrl = baseUrl + "?" + queryString + "&vnp_SecureHash=" + vnp_SecureHash;
+            var querystring = data.ToString();
 
-            return paymentUrl;
+            baseUrl += "?" + querystring;
+            var signData = querystring;
+            if (signData.Length > 0)
+            {
+                signData = signData.Remove(data.Length - 1, 1);
+            }
+
+            var vnpSecureHash = Utils.HmacSHA512(vnpHashSecret, signData);
+            baseUrl += "vnp_SecureHash=" + vnpSecureHash;
+
+            return baseUrl;
         }
+        #endregion
 
+        #region Response process
         public bool ValidateSignature(string inputHash, string secretKey)
         {
-            string rspRaw = GetResponseData();
-            string myChecksum = Utils.HmacSHA512(secretKey, rspRaw);
+            var rspRaw = GetResponseData();
+            var myChecksum = Utils.HmacSHA512(secretKey, rspRaw);
             return myChecksum.Equals(inputHash, StringComparison.InvariantCultureIgnoreCase);
         }
 
         private string GetResponseData()
         {
-            StringBuilder data = new StringBuilder();
+            var data = new StringBuilder();
             if (_responseData.ContainsKey("vnp_SecureHashType"))
             {
                 _responseData.Remove("vnp_SecureHashType");
             }
+
             if (_responseData.ContainsKey("vnp_SecureHash"))
             {
                 _responseData.Remove("vnp_SecureHash");
             }
-            foreach (KeyValuePair<string, string> kv in _responseData)
+
+            foreach (var (key, value) in _responseData.Where(kv => !string.IsNullOrEmpty(kv.Value)))
             {
-                if (data.Length > 0)
-                {
-                    data.Append("&");
-                }
-                data.Append(kv.Key + "=" + WebUtility.UrlEncode(kv.Value));
+                data.Append(WebUtility.UrlEncode(key) + "=" + WebUtility.UrlEncode(value) + "&");
             }
+
+            //remove last '&'
+            if (data.Length > 0)
+            {
+                data.Remove(data.Length - 1, 1);
+            }
+
             return data.ToString();
+        }
+        #endregion
+
+    }
+
+    public class Utils
+    {
+        public static string HmacSHA512(string key, string inputData)
+        {
+            var hash = new StringBuilder();
+            var keyBytes = Encoding.UTF8.GetBytes(key);
+            var inputBytes = Encoding.UTF8.GetBytes(inputData);
+            using (var hmac = new HMACSHA512(keyBytes))
+            {
+                var hashValue = hmac.ComputeHash(inputBytes);
+                foreach (var theByte in hashValue)
+                {
+                    hash.Append(theByte.ToString("x2"));
+                }
+            }
+
+            return hash.ToString();
+        }
+
+
+        // có chế biến cho .NET Core MVC
+        public static string GetIpAddress(HttpContext context)
+        {
+            var ipAddress = string.Empty;
+            try
+            {
+                var remoteIpAddress = context.Connection.RemoteIpAddress;
+
+                if (remoteIpAddress != null)
+                {
+                    if (remoteIpAddress.AddressFamily == AddressFamily.InterNetworkV6)
+                    {
+                        remoteIpAddress = Dns.GetHostEntry(remoteIpAddress).AddressList
+                            .FirstOrDefault(x => x.AddressFamily == AddressFamily.InterNetwork);
+                    }
+
+                    if (remoteIpAddress != null) ipAddress = remoteIpAddress.ToString();
+
+                    return ipAddress;
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Invalid IP:" + ex.Message;
+            }
+
+            return "127.0.0.1";
         }
     }
 
@@ -92,22 +159,5 @@ namespace MyThuatShop.Helpers
         }
     }
 
-    public static class Utils
-    {
-        public static string HmacSHA512(string key, string inputData)
-        {
-            var hash = new StringBuilder();
-            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
-            byte[] inputBytes = Encoding.UTF8.GetBytes(inputData);
-            using (var hmac = new HMACSHA512(keyBytes))
-            {
-                byte[] hashValue = hmac.ComputeHash(inputBytes);
-                foreach (var theByte in hashValue)
-                {
-                    hash.Append(theByte.ToString("x2"));
-                }
-            }
-            return hash.ToString();
-        }
+
     }
-}
